ec6e6e546b14447b7b646bfa97678bd5bc9dbdb5
N0tExpectErr0r <871731342@qq.com>
2018-07-20 19:43:13 +0800

    feat(MainActivity):完成了修改图片颜色功能

---
+++ app/src/main/java/com/ima_vida/vidaeditor/ImageUtil.java
@@ -0,0 +1,43 @@
+package com.ima_vida.vidaeditor;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffColorFilter;
+import android.util.Log;
+
+/**
+ * @AUTHOR nullptr
+ * @DATE 创建时间: 2018/7/20
+ * @DESCRIPTION
+ */
+public class ImageUtil {
+    public static Bitmap handleBitmapColor(Bitmap inBitmap , int tintColor) {
+        if (inBitmap == null) {
+            return null;
+        }
+        Bitmap outBitmap = Bitmap.createBitmap (inBitmap.getWidth(), inBitmap.getHeight() , inBitmap.getConfig());
+        Canvas canvas = new Canvas(outBitmap);
+        Paint paint = new Paint();
+        paint.setColorFilter( new PorterDuffColorFilter(tintColor, PorterDuff.Mode.SRC_IN)) ;
+        canvas.drawBitmap(inBitmap , 0, 0, paint) ;
+        return outBitmap ;
+    }
+
+    public static Bitmap setBitmapSize(Bitmap bitmap, int newWidth, int newHeight) {
+        int width = bitmap.getWidth();
+        int height = bitmap.getHeight();
+        Log.d("ImageUtil","originSize:"+width+"*"+height);
+        Log.d("ImageUtil","newSize:"+newWidth+"*"+newHeight);
+        float scaleWidth = ((float) newWidth) / width;
+        float scaleHeight = ((float) newHeight) / height;
+        Matrix matrix = new Matrix();
+        matrix.postScale(scaleWidth, scaleHeight);
+        return Bitmap.createBitmap(bitmap, 0, 0, width, height, matrix, true);
+    }
+}
--- app/src/main/java/com/ima_vida/vidaeditor/MainActivity.java
+++ app/src/main/java/com/ima_vida/vidaeditor/MainActivity.java
@@ -1,13 +1,87 @@
 package com.ima_vida.vidaeditor;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import static android.graphics.Color.rgb;
 
-public class MainActivity extends AppCompatActivity {
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
+
+    private Bitmap mOriBitmap;
+
+    private int red;
+    private int green;
+    private int blue;
+
+    private ImageView mIvVida;
+
+    private TextView mTvRed;
+    private TextView mTvGreen;
+    private TextView mTvBlue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mIvVida = findViewById(R.id.iv_vida);
+
+        SeekBar sbRed = findViewById(R.id.sb_red);
+        SeekBar sbGreen = findViewById(R.id.sb_green);
+        SeekBar sbBlue = findViewById(R.id.sb_blue);
+
+        mTvRed = findViewById(R.id.tv_red);
+        mTvGreen = findViewById(R.id.tv_green);
+        mTvBlue = findViewById(R.id.tv_blue);
+
+        sbRed.setOnSeekBarChangeListener(this);
+        sbGreen.setOnSeekBarChangeListener(this);
+        sbBlue.setOnSeekBarChangeListener(this);
+
+        red = 0;
+        green = 211;
+        blue = 254;
+        //ImageView初始化完成后，加载图片
+        mIvVida.post(new Runnable() {
+            @Override
+            public void run() {
+                Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.vida);
+                mOriBitmap = ImageUtil.setBitmapSize(bitmap,mIvVida.getMeasuredWidth(),mIvVida.getMeasuredHeight());
+                int color = rgb(red, green, blue);
+                mIvVida.setImageBitmap(ImageUtil.handleBitmapColor(mOriBitmap, color));
+            }
+        });
     }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        switch (seekBar.getId()) {
+            case R.id.sb_red:
+                red = progress;
+                mTvRed.setText(progress + "");
+                break;
+            case R.id.sb_green:
+                green = progress;
+                mTvGreen.setText(progress + "");
+                break;
+            case R.id.sb_blue:
+                blue = progress;
+                mTvBlue.setText(progress + "");
+                break;
+            default:
+                break;
+        }
+        int color = rgb(red, green, blue);
+        mIvVida.setImageBitmap(ImageUtil.handleBitmapColor(mOriBitmap, color));
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {}
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {}
 }
